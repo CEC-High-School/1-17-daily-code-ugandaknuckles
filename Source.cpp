3d4f5dbf3b616ab86c31961728579810413209b9
ugandaknuckles <35495803+ugandaknuckles@users.noreply.github.com>
2018-01-17 08:19:38 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Windows.H>
+using namespace std;
+int main(){
+
+	cout <<"What did the yoga instructor say when her landlord tried to evict her?(4)"<< endl;
+	
+	Sleep(3000);
+
+	cout << "Namaste" << endl;
+
+
+}
